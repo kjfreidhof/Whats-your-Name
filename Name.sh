c935e384df54e37406f6ac538fd9082b99ee68af
kjfreidhof <89760217+kjfreidhof@users.noreply.github.com>
2023-02-04 00:27:54 -0500

    Add files via upload

---
+++ Name.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo What is your name?
+read MY_NAME
+echo "Hello $MY_NAME - hope you are doing great?"
+
+echo "What is your name?"
+read MyName
+echo "My Name is $MyName"
+echo "Nice to meet you $MyName"
+sleep 3
+echo "What are you doing today $MY_NAME"
+sleep 3
+
+echo "Not Much $MyName"
+sleep 3
+echo "Just creating a shell script"
+sleep 3
+
+echo "Thats cools $MY_NAME i love shell scripts"
+sleep 3
+echo "what does your shell script do $MY_NAME"
+sleep 3
+
+echo "Its prints out words $MyName"
+sleep 3
+
+echo "Cool $MY_NAME"
+sleep 3
+echo "Bye $MY_NAME"
+sleep 3
+
+echo "Bye $MyName"
